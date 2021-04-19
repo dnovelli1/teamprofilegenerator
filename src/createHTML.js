722f69c90f007e1ae02e5baeddd6a1eb5292d34f
Jake Novelli <jakenovelli11@gmail.com>
2021-04-18 18:50:20 -0700

    added meta's and changed bootstrap link

--- src/createHTML.js
+++ src/createHTML.js
@@ -5,8 +5,11 @@ function createHTML(data) {
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
         <title>Team Generator</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     </head>
     <body>
         <style>
