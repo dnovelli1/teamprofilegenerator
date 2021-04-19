940c4cdf7b947d26edd2169669d8d3b9215dd03f
Jake Novelli <jakenovelli11@gmail.com>
2021-04-18 18:48:14 -0700

    added base of html and template literal for all functions to generate the type of card user wants

--- src/createHTML.js
+++ src/createHTML.js
@@ -1,81 +1,134 @@
 const fs = require('fs');
 
+function createHTML(data) {
+    return `
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Team Generator</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    </head>
+    <body>
+        <style>
+        body {
+            box-sizing: border-box;
+        }
+        .container {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-between;
+        }
+        #project {
+            background: cornflowerblue;
+            color: azure;
+        }
+        #person {
+            background: cornflowerblue;
+            color: azure;
+        }
+        .subject {
+            font-size: larger;
+            font-weight: bolder;
+        }
+        </style>
+        <div class="jumbotron" id="project" >
+            <h1 class="text-center">Development Team</h1>
+        </div>
+        <div class="container">
+            ${allCards(data)}
+        </div>
+    </body>
+</html>
+  `
+}
 
-
-
-
+function allCards (data) {
+    return data.map((data) => {
+        let role = data.getRole();
+        switch (role) {
+            case "Manager":
+                return createManager(data);
+            break;
+            case "Intern":
+                return createIntern(data);
+            break;
+            case "Engineer":
+                return createEngineer(data);
+            break;
+        }
+    }).join("\n");
+}
 
 
 
 function createManager(data) {
     return `
-    <div class= "card cardbody">
-        <div class= "card-header" id= "tab">
-            <p class="name"${data.name}</p>
-            <div>${data.getRole()}</div>
-        </div>
-        <div class= "card-body">
-            <form role= "form">
-                <div class= "form-group">
-                    <label>ID: ${data.id}<label>
+            <div class= "card cardbody">
+                <div class= "card-header" id= "person">
+                    <p class="subject">${data.name}</p>
+                    <div>${data.getRole()}</div>
                 </div>
-                <div class= "form-group">
-                    <label>Email: <a href="mailto:${data.email}">${data.email}</a></label>
+                <div class= "card-body">
+                    <form role= "form">
+                        <div class= "form-group">
+                            <label>ID: ${data.id}<label>
+                        </div>
+                        <div class= "form-group">
+                            <label>Email: <a href="mailto:${data.email}">${data.email}</a></label>
+                        </div>
+                        <div class= "form-group">
+                            <label>Office Number: ${data.officeNumber}</label>
+                        </div>
+                    </form>
                 </div>
-                <div class= "form-group">
-                    <label>Office Number: ${data.officeNumber}</label>
-                </div>
-            </form>
-        </div>
-    </div>
-    `
+            </div>`
 }
 
 
 function createIntern(data) {
      return `
-    <div class= "card cardbody">
-        <div class= "card-header" id= "tab">
-            <p class="name"${data.name}</p>
-            <div>${data.getRole()}</div>
-        </div>
-        <div class= "card-body">
-            <form role= "form">
-                <div class= "form-group">
-                    <label>ID: ${data.id}<label>
+            <div class= "card cardbody">
+                <div class= "card-header" id= "person">
+                    <p class="subject">${data.name}</p>
+                    <div>${data.getRole()}</div>
                 </div>
-                <div class= "form-group">
-                    <label>Email: <a href="mailto:${data.email}">${data.email}</a></label>
+                <div class= "card-body">
+                    <form role= "form">
+                        <div class= "form-group">
+                            <label>ID: ${data.id}<label>
+                        </div>
+                        <div class= "form-group">
+                            <label>Email: <a href="mailto:${data.email}">${data.email}</a></label>
+                        </div>
+                        <div class= "form-group">
+                            <label>School: ${data.school}</label>
+                        </div>
+                    </form>
                 </div>
-                <div class= "form-group">
-                    <label>School: ${data.school}</label>
-                </div>
-            </form>
-        </div>
-    </div>
-    `
+            </div>`
 }
 
 function createEngineer(data) {
     return `
-    <div class= "card cardbody">
-        <div class= "card-header" id= "tab">
-            <p class="name"${data.name}</p>
-            <div>${data.getRole()}</div>
-        </div>
-        <div class= "card-body">
-            <form role= "form">
-                <div class= "form-group">
-                    <label>ID: ${data.id}<label>
+            <div class= "card cardbody">
+                <div class= "card-header" id= "person">
+                    <p class="subject">${data.name}</p>
+                    <div>${data.getRole()}</div>
                 </div>
-                <div class= "form-group">
-                    <label>Email: <a href="mailto:${data.email}">${data.email}</a></label>
+                <div class= "card-body">
+                    <form role= "form">
+                        <div class= "form-group">
+                            <label>ID: ${data.id}<label>
+                        </div>
+                        <div class= "form-group">
+                            <label>Email: <a href="mailto:${data.email}">${data.email}</a></label>
+                        </div>
+                        <div class= "form-group">
+                            <label>GitHub: <a href="https://github.com/${data.github}">GitHub</a></label>
+                        </div>
+                    </form>
                 </div>
-                <div class= "form-group">
-                    <label>GitHub: <a href="https://github.com/${data.github}"></a></label>
-                </div>
-            </form>
-        </div>
-    </div>
-    `
+            </div>`
 }
+
+module.exports = createHTML;
